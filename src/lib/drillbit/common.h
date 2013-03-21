9728ffa3b356de6e1d6e4ac3dd9e85a40d63b7f0
Johannes Ebke <johannes@ebke.org>
2013-03-21 20:26:49 +0100

    Add more unused template metacode to common.h

--- src/lib/drillbit/common.h
+++ src/lib/drillbit/common.h
@@ -1,9 +1,14 @@
+#ifndef DRILLBIT_COMMON_H_
+#define DRILLBIT_COMMON_H_
+
+#include <vector>
+
 #include <google/protobuf/wire_format_lite.h>
 
 using google::protobuf::internal::WireFormatLite;
 
 template<enum WireFormatLite::CppType cpp_type>
-class CppTypeOf {
+class TypeFromCppType {
     typedef typename std::conditional<cpp_type == WireFormatLite::CPPTYPE_INT32, int32_t, 
     typename std::conditional<cpp_type == WireFormatLite::CPPTYPE_UINT32, uint32_t, 
     typename std::conditional<cpp_type == WireFormatLite::CPPTYPE_FLOAT, float, 
@@ -17,4 +22,31 @@ class CppTypeOf {
     >::type>::type>::type>::type>::type>::type>::type>::type>::type type;
 };
 
+template<enum WireFormatLite::FieldType field_type>
+class TypeFromFieldType {
+#define cpp_type WireFormatLite::FieldTypeToCppType(field_type)
+    typedef typename std::conditional<cpp_type == WireFormatLite::CPPTYPE_INT32, int32_t, 
+    typename std::conditional<cpp_type == WireFormatLite::CPPTYPE_UINT32, uint32_t, 
+    typename std::conditional<cpp_type == WireFormatLite::CPPTYPE_FLOAT, float, 
+    typename std::conditional<cpp_type == WireFormatLite::CPPTYPE_BOOL, bool, 
+    typename std::conditional<cpp_type == WireFormatLite::CPPTYPE_ENUM, int, 
+    typename std::conditional<cpp_type == WireFormatLite::CPPTYPE_INT64, int64_t, 
+    typename std::conditional<cpp_type == WireFormatLite::CPPTYPE_UINT64, uint64_t, 
+    typename std::conditional<cpp_type == WireFormatLite::CPPTYPE_DOUBLE, double, 
+    typename std::conditional<cpp_type == WireFormatLite::CPPTYPE_STRING, std::string, 
+    void* 
+    >::type>::type>::type>::type>::type>::type>::type>::type>::type type;
+#undef cpp_type
+};
 
+template<typename T, unsigned int L>
+class NestedVector {
+    static_assert(L > 0 and L < 4, "nested vector length not in range");
+    typedef typename std::conditional<L==1, std::vector<T>,
+            typename std::conditional<L==2, std::vector<std::vector<T>>,
+            typename std::conditional<L==3, std::vector<std::vector<std::vector<T>>>,
+            std::vector<std::vector<std::vector<std::vector<T>>>>
+            >::type>::type>::type type;
+};
+
+#endif
