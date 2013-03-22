4f1c150c0b58c8b80dafc97bffe5c8fe7d5cb490
Johannes Ebke <johannes@ebke.org>
2013-03-22 10:31:05 +0100

    Amend lua to not compile with readline

--- waf-tools/packlua.py
+++ waf-tools/packlua.py
@@ -6,7 +6,10 @@ C2 = '#YYY'
 if not os.path.exists("lua-5.2.1.tar.gz"):
     assert 0 == os.system("wget http://www.lua.org/ftp/lua-5.2.1.tar.gz")
 assert 0 == os.system("tar zxf lua-5.2.1.tar.gz")
-lua = file("lua-5.2.1.tar.gz").read()
+assert 0 == os.system("sed -i s/LUA_USE_READLINE/LUA_DONT_USE_READLINE/  lua-5.2.1/src/luaconf.h")
+assert 0 == os.system("tar zcf lua-5.2.1-mod.tar.gz lua-5.2.1")
+
+lua = file("lua-5.2.1-mod.tar.gz").read()
 o = file("lua.py", "wb")
 o.write(file("lua.py.input").read())
 o.write("#==>\n#")
