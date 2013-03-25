1feb69926103ae829ec4e9f326f3d76a59be2b0b
Johannes Ebke <johannes@ebke.org>
2013-03-25 18:37:24 +0100

    Remove one preprocessor macro using a constexpr

--- src/lib/drillbit/common.h
+++ src/lib/drillbit/common.h
@@ -6,29 +6,6 @@
 #include <google/protobuf/wire_format_lite.h>
 using google::protobuf::internal::WireFormatLite;
 
-// Redeclare kFieldTypeToCppTypeMap to be constexpr
-constexpr WireFormatLite::CppType kFieldTypeToCppTypeMap[WireFormatLite::MAX_FIELD_TYPE + 1] = {
-  static_cast<WireFormatLite::CppType>(0),  // 0 is reserved for errors
-  WireFormatLite::CPPTYPE_DOUBLE,   // TYPE_DOUBLE
-  WireFormatLite::CPPTYPE_FLOAT,    // TYPE_FLOAT
-  WireFormatLite::CPPTYPE_INT64,    // TYPE_INT64
-  WireFormatLite::CPPTYPE_UINT64,   // TYPE_UINT64
-  WireFormatLite::CPPTYPE_INT32,    // TYPE_INT32
-  WireFormatLite::CPPTYPE_UINT64,   // TYPE_FIXED64
-  WireFormatLite::CPPTYPE_UINT32,   // TYPE_FIXED32
-  WireFormatLite::CPPTYPE_BOOL,     // TYPE_BOOL
-  WireFormatLite::CPPTYPE_STRING,   // TYPE_STRING
-  WireFormatLite::CPPTYPE_MESSAGE,  // TYPE_GROUP
-  WireFormatLite::CPPTYPE_MESSAGE,  // TYPE_MESSAGE
-  WireFormatLite::CPPTYPE_STRING,   // TYPE_BYTES
-  WireFormatLite::CPPTYPE_UINT32,   // TYPE_UINT32
-  WireFormatLite::CPPTYPE_ENUM,     // TYPE_ENUM
-  WireFormatLite::CPPTYPE_INT32,    // TYPE_SFIXED32
-  WireFormatLite::CPPTYPE_INT64,    // TYPE_SFIXED64
-  WireFormatLite::CPPTYPE_INT32,    // TYPE_SINT32
-  WireFormatLite::CPPTYPE_INT64,    // TYPE_SINT64
-};
-
 template<enum WireFormatLite::CppType cpp_type>
 class TypeFromCppType {
  public:
@@ -48,7 +25,29 @@ class TypeFromCppType {
 template<enum WireFormatLite::FieldType field_type>
 class TypeFromFieldType {
  public:
-#define cpp_type kFieldTypeToCppTypeMap[field_type]
+    // Redeclare kFieldTypeToCppTypeMap to be constexpr
+    static constexpr WireFormatLite::CppType kFieldTypeToCppTypeMap[WireFormatLite::MAX_FIELD_TYPE + 1] = {
+      static_cast<WireFormatLite::CppType>(0),  // 0 is reserved for errors
+      WireFormatLite::CPPTYPE_DOUBLE,   // TYPE_DOUBLE
+      WireFormatLite::CPPTYPE_FLOAT,    // TYPE_FLOAT
+      WireFormatLite::CPPTYPE_INT64,    // TYPE_INT64
+      WireFormatLite::CPPTYPE_UINT64,   // TYPE_UINT64
+      WireFormatLite::CPPTYPE_INT32,    // TYPE_INT32
+      WireFormatLite::CPPTYPE_UINT64,   // TYPE_FIXED64
+      WireFormatLite::CPPTYPE_UINT32,   // TYPE_FIXED32
+      WireFormatLite::CPPTYPE_BOOL,     // TYPE_BOOL
+      WireFormatLite::CPPTYPE_STRING,   // TYPE_STRING
+      WireFormatLite::CPPTYPE_MESSAGE,  // TYPE_GROUP
+      WireFormatLite::CPPTYPE_MESSAGE,  // TYPE_MESSAGE
+      WireFormatLite::CPPTYPE_STRING,   // TYPE_BYTES
+      WireFormatLite::CPPTYPE_UINT32,   // TYPE_UINT32
+      WireFormatLite::CPPTYPE_ENUM,     // TYPE_ENUM
+      WireFormatLite::CPPTYPE_INT32,    // TYPE_SFIXED32
+      WireFormatLite::CPPTYPE_INT64,    // TYPE_SFIXED64
+      WireFormatLite::CPPTYPE_INT32,    // TYPE_SINT32
+      WireFormatLite::CPPTYPE_INT64,    // TYPE_SINT64
+    };
+    static constexpr enum WireFormatLite::CppType cpp_type = kFieldTypeToCppTypeMap[static_cast<const int>(field_type)];
     typedef typename std::conditional<cpp_type == WireFormatLite::CPPTYPE_INT32, int32_t, 
     typename std::conditional<cpp_type == WireFormatLite::CPPTYPE_UINT32, uint32_t, 
     typename std::conditional<cpp_type == WireFormatLite::CPPTYPE_FLOAT, float, 
@@ -60,7 +59,6 @@ class TypeFromFieldType {
     typename std::conditional<cpp_type == WireFormatLite::CPPTYPE_STRING, std::string, 
     void* 
     >::type>::type>::type>::type>::type>::type>::type>::type>::type type;
-#undef cpp_type
 };
 
 template<typename T, unsigned int L>
