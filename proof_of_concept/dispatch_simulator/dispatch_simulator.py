da6373499706a4e8abe874a267352f2e4fab550c
Johannes Ebke <johannes@ebke.org>
2013-10-18 14:40:13 +0200

    Added more tests and sped up the dispatch simulator

--- proof_of_concept/dispatch_simulator/dispatch_simulator.py
+++ proof_of_concept/dispatch_simulator/dispatch_simulator.py
@@ -22,7 +22,7 @@ class DirectoryDitPack(object):
 
 class EmptyDitPack(DirectoryDitPack):
     def __init__(self, columns, tablets):
-        self.columns, self.tablets = columns, tablets
+        self.columns, self.tablets = sorted(columns), sorted(tablets)
         self._ditfiles = ["foo"]#["missingdata({0}, {1})".format(tablets, c)
                                 #for c in columns]
         self._current_tablet_offset = defaultdict(lambda: -1)
@@ -35,7 +35,6 @@ class DitPackSet(object):
         self.seen_tablets = set()
         self.seen_columns = set()
         self.packset_for_column = defaultdict(set)
-        self.columnset_for_packset = defaultdict(set)
 
         for pack in self._packs:
             self.seen_tablets |= set(pack.tablets)
@@ -43,9 +42,12 @@ class DitPackSet(object):
             for column in pack.columns:
                 self.packset_for_column[column].add(pack)
 
+    def columnset_for_packset(self):
+        columnset_for_packset = defaultdict(set)
         for column in self.columns:
             packs = frozenset(self.packset_for_column[column])
-            self.columnset_for_packset[packs].add(column)
+            columnset_for_packset[packs].add(column)
+        return columnset_for_packset
 
     @property
     def columns(self):
@@ -56,7 +58,7 @@ class DitPackSet(object):
         return sorted(self.seen_tablets)
 
     def missing_tablets_by_columnset(self):
-        for packset, columnset in sorted(self.columnset_for_packset.iteritems()):
+        for packset, columnset in sorted(self.columnset_for_packset().iteritems()):
             tablets_for_columnset = set().union(*[p.tablets for p in packset])
             missing = self.seen_tablets - tablets_for_columnset
             if missing:
@@ -64,18 +66,18 @@ class DitPackSet(object):
 
     def add(self, pack):
         for column in pack.columns:
-            self.packset_for_column[column] = set((pack))
-        self.columnset_for_packset[frozenset((pack))] = frozenset(pack.columns)
+            self.packset_for_column[column].add(pack)
     
     def add_missing(self):
         for columnset, tablets in self.missing_tablets_by_columnset():
-            print "Missing tablet ids {0} for {1}".format(tablets, sorted(columnset))
+            print "{0} tablets missing for {1} columns".format(len(tablets), len(columnset))
+            #print "Missing tablet ids {0} for {1}".format(tablets, sorted(columnset))
             fp = EmptyDitPack(columnset, tablets)
             self.add(fp)
 
     def ordered_packs_by_columnset(self):
         tablets_packs_by_columnset = {}
-        for packs, column_set in self.columnset_for_packset.iteritems():
+        for packs, column_set in self.columnset_for_packset().iteritems():
             tablet_pack = []
             for pack in packs:
                 tablet_pack.extend((tablet, pack) for tablet in pack.tablets)
@@ -84,6 +86,13 @@ class DitPackSet(object):
         return tablets_packs_by_columnset
 
     def readers_by_tablet(self):
+        missing = self.missing_tablets_by_columnset()
+        if missing:
+            self.add_missing()
+            #for columnset, tablets in missing:
+                #print "Missing tablet ids {0} for {1}".format(tablets, sorted(columnset))
+            #raise Exception()
+
         ordered_packs_by_columnset = self.ordered_packs_by_columnset()
         for tablet in sorted(self.tablets):
             readers = {}
@@ -91,8 +100,7 @@ class DitPackSet(object):
                 while True:
                     read_tablet, pack = pack_stack.pop()
                     if read_tablet == tablet:
-                        for column in columnset:
-                            readers[column] = pack.next_reader(column)
+                        readers[columnset] = pack.next_reader(columnset)
                         break
                     elif read_tablet > tablet:
                         # skip next reader
--- proof_of_concept/dispatch_simulator/test_dispatch_simulator.py
+++ proof_of_concept/dispatch_simulator/test_dispatch_simulator.py
@@ -16,17 +16,40 @@ def test_simple():
         pprint(readers)
 
 def test_massive():
-    input_packs = [EmptyDitPack(as_cols(range(10000)), as_tabs(range(1000)))]
+    input_packs = [EmptyDitPack(as_cols(range(10000)), as_tabs(range(1000*1000)))]
     packset = DitPackSet(input_packs)
 
     for i, (tablet, readers) in enumerate(packset.readers_by_tablet()):
-        if i%1000 == 0:
+        if i%100000 == 0:
+            print i, tablet
+            pass
+
+def test_multi():
+    input_packs = [EmptyDitPack(as_cols(range(0,  5)), as_tabs(range(0, 5))),
+                   EmptyDitPack(as_cols(range(6, 10)), as_tabs(range(0, 5))),
+                   EmptyDitPack(as_cols(range(0,  5)), as_tabs(range(5, 10))),
+                   EmptyDitPack(as_cols(range(5, 10)), as_tabs(range(5, 10)))]
+    packset = DitPackSet(input_packs)
+
+    for tablet, readers in packset.readers_by_tablet():
+        print tablet
+        pprint(readers)
+
+def test_multi_massive():
+    n_tablets = 1000*1000
+    input_packs = [EmptyDitPack(as_cols(range(0,  5000)), as_tabs(range(0, n_tablets))),
+                   EmptyDitPack(as_cols(range(5010, 10000)), as_tabs(range(0, n_tablets))),
+                   EmptyDitPack(as_cols(range(0,  4000)), as_tabs(range(n_tablets, 2*n_tablets))),
+                   EmptyDitPack(as_cols(range(4010, 10000)), as_tabs(range(n_tablets, 2*n_tablets)))]
+    packset = DitPackSet(input_packs)
+
+    for i, (tablet, readers) in enumerate(packset.readers_by_tablet()):
+        if i%100000 == 0:
             print i, tablet
             pass
 
 def main():
-    test_simple()
-    test_massive()
+    test_multi_massive()
 
 if __name__ == "__main__":
     main()
